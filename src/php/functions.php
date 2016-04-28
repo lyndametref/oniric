0584c75c81e4ed2800841ff50ce3ab0f79c6222b
Lynda Metref <lynda@email.com>
2016-04-28 15:04:33 +0200

    enable dropdown menu

--- src/php/functions.php
+++ src/php/functions.php
@@ -16,17 +16,9 @@ register_sidebars(1, array('name' => 'Navbar Widgets Area'));
 register_sidebars(3, array('name' => 'Footer %d Widgets Area'));
 
 // Add feature support
-add_theme_support( 'post-thumbnails' ); 
+add_theme_support( 'post-thumbnails' );
 
-// Advanced Custom Fields Fuctions
-function convert_field_to_array ($f) { return explode("</p>", get_field($f)); }
-function put_in_paragraph ($p) { return "<p>" . $p . "</p>"; }
-function print_first_half ($a) { for($i = 0; $i < ceil(count($a)/2); $i++) { echo put_in_paragraph($a[$i]); } }
-function print_second_half ($a) { for($i = ceil(count($a)/2); $i < count($a); $i++) { echo put_in_paragraph($a[$i]); } }
-function convert_field_and_print_column ($field, $column="first") {
-	$field = convert_field_to_array($field);
-	if ($column == "first") print_first_half($field);
-	else print_second_half($field);
-}
-
-?>
+// Enable Bootstrap dropdown menu
+// https://github.com/twittem/wp-bootstrap-navwalker
+// WARNING: Dependency not automatised!!!!!
+require_once('wp_bootstrap_navwalker.php');
