4a8ef484c7f282c0f6b025581d8b8f6c7f2d797b
Lynda Metref <lynda@email.com>
2016-04-28 15:49:21 +0200

    one more footer at the bottom

--- src/php/functions.php
+++ src/php/functions.php
@@ -13,7 +13,7 @@ register_nav_menus( array(
 // register several widget areas in the theme
 register_sidebars(1, array('name' => 'Sidebar Widget Area'));
 register_sidebars(1, array('name' => 'Navbar Widgets Area'));
-register_sidebars(3, array('name' => 'Footer %d Widgets Area'));
+register_sidebars(4, array('name' => 'Footer %d Widgets Area'));
 
 // Add feature support
 add_theme_support( 'post-thumbnails' );
