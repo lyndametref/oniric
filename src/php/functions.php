34f80b8d60215ae5e959f613151c1eb5a86eef0f
Lynda Metref <lynda@email.com>
2016-05-01 17:50:39 +0200

    move the computation of variables in functions.php create array with variable and use it in archive template

--- src/php/functions.php
+++ src/php/functions.php
@@ -23,3 +23,18 @@ add_theme_support( 'post-thumbnails' );
 // https://github.com/twittem/wp-bootstrap-navwalker
 // WARNING: Dependency not automatised!!!!!
 require_once('wp_bootstrap_navwalker.php');
+
+// Generate constants to query post on the archive page
+// and display a paginated result
+function init_archive_display_var() {
+	$paged = get_query_var('paged') ? get_query_var('paged') : 1;
+	$total_posts = wp_count_posts()->publish;
+	$posts_per_page = get_option('posts_per_page');
+	$offset = ($paged-1)*$posts_per_page;
+	$page_number = ceil($total_posts/$posts_per_page);
+	return array("paged" => $paged,
+		"total_posts" => $total_posts,
+		"posts_per_page" => $posts_per_page,
+		"offset" => $offset,
+		"page_number" => $page_number);
+}
